3c0976884b3dee0ea18fa5d7e0ad22684da07b55
saurabhthakre1721999 <saurabhthakre1721999@gmail.com>
2024-05-16 14:54:08 +0530

    native android

---
+++ android/test 1/vite-project/src/component/home.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function Home() {
+  return (
+    <>
+      <div>
+        <h1> Home </h1>
+      </div>
+    </>
+  );
+}
+
+export default Home;
